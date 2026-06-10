960cf56d1ff66e9e68cdc3f177331d83c09b7cec
Carine-2025 <carinesilvaga@gmail.com>
2026-06-10 16:55:50 -0300

    feat: adiciona consulta de usuário por email e senha no repositório

--- projeto_tcc/src/main/java/br/com/criandoapi/projeto_tcc/repository/UsuarioRepository.java
+++ projeto_tcc/src/main/java/br/com/criandoapi/projeto_tcc/repository/UsuarioRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import br.com.criandoapi.projeto_tcc.model.Usuario;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, Integer>{
+	
+	Usuario findByEmailAndSenha(String email, String senha);
 
 }
